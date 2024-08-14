710174ecc7cb88f34076659a6ea07f7d36c480b9
Jin19121 <jinkim19121@gmail.com>
2024-08-14 14:58:10 +0900

    변수명 작성 관습

---
+++ src/ch02/lecture/p1variable/C02Variable.java
@@ -0,0 +1,10 @@
+package ch02.lecture.p1variable;
+
+public class C02Variable{
+    public static void main(String[] args) {
+        int age;
+//        관습상 안되는 경우: int Age | int AGE | int aGE
+//        여러 단어 조합 시 각 단어 앞만 대문자 camelCase ex) int numberOfStudents (첫단어 첫글자는 소문자)
+//        파이썬은 snake_case
+    }
+}
