168e354a41e905e803064f5494ba1c16fc878ab3
Jin19121 <jinkim19121@gmail.com>
2024-08-19 09:45:50 +0900

    print

--- src/ch02/sec06/StringExample1.java
+++ src/ch02/sec06/StringExample1.java
@@ -1,8 +1,6 @@
 package ch02.sec06;
 
-import java.sql.SQLOutput;
-
-public class StringExample {
+public class StringExample1 {
     public static void main(String[] args) {
         String name = "DK";
         String job = "singer";
@@ -11,6 +9,6 @@ public class StringExample {
 
         String str = "나는 \"춤과 노래\"를 배웁니다.";
         System.out.println(str);
-        
+
     }
 }
