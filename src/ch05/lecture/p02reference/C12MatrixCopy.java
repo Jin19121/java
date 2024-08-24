dc04d92316a2e3f7a40cc629e14541c61df28199
Jin19121 <jinkim19121@gmail.com>
2024-08-24 17:49:19 +0900

    review

--- src/ch05/lecture/p02reference/C12MatrixCopy.java
+++ src/ch05/lecture/p02reference/C12MatrixCopy.java
@@ -5,16 +5,27 @@ import java.util.Arrays;
 public class C12MatrixCopy {
     public static void main(String[] args) {
         int[][] m1 = {{98, 76, 65}, {54, 21, 10}};
-        //m2에 m1을 복사하는 코드
 
+        //m2에 m1을 복사하는 코드
         int[][] m2 = new int[m1.length][];
 
         for (int i = 0; i < m1.length; i++) {
-            m2[i] = m1[i];
+            int[] row = new int[m1[i].length];
+            for (int j = 0; j < m1[i].length; j++) {
+                row[j] = m1[i][j];
+            }
+            m2[i] = row;
         }
+
+        System.out.println("복사 후 m2");
         System.out.println(Arrays.toString(m2[0]));
+        System.out.println(Arrays.toString(m2[1]));
+
         m2[0][0] = 1010;
         System.out.println(m1[0][0]);//깊은 복사
-//? 확인 요망
+
+        System.out.println("m2[0][0]을 1010으로 바꾼 후 m2");
+        System.out.println(Arrays.toString(m2[0]));
+        System.out.println(Arrays.toString(m2[1]));
     }
 }
