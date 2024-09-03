49f01c94b4e43a9c241cee9b8b207aa8f33d2fcf
Jin19121 <jinkim19121@gmail.com>
2024-09-03 18:00:15 +0900

    lambda review 240903

--- src/ch16/lecture/p02method_reference/C05ConstructorReference.java
+++ src/ch16/lecture/p02method_reference/C05ConstructorReference.java
@@ -5,12 +5,7 @@ public class C05ConstructorReference {
     MyInterface51 o = () -> new MyClass51();
     MyInterface51 p = MyClass51::new;
 
-    MyInterface51 q = new MyInterface51() {
-        @Override
-        public void some() {
-            new String();
-        }
-    };
+    MyInterface51 q = () -> new String();
     MyInterface51 r = String::new;
 
     MyInterface52 s = a -> new MyClass51(a);
