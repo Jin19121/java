8b16748c8a2fed33ba81cf23c5e27c7f32ca7da1
Jin19121 <jinkim19121@gmail.com>
2024-09-21 17:56:46 +0900

    정리중

--- src/ch06/lecture/p02field/C01Field.java
+++ src/ch06/lecture/p02field/C01Field.java
@@ -5,15 +5,16 @@ public class C01Field {
         MyClass01 a = new MyClass01();
         MyClass01 b = new MyClass01();
         //객체명.필드명
-        System.out.println("a.age = " + a.age);
-        System.out.println("b.age = " + b.age);
+        System.out.println("a.age = " + a.age); //5
+        System.out.println("b.age = " + b.age); //5
         System.out.println("a.name = " + a.name);
         System.out.println("b.name = " + b.name);
 
         a.age = 55;
 
-        System.out.println("a.age = " + a.age);//서로 다른 인스턴스
-        System.out.println("b.age = " + b.age);
+        //서로 다른 인스턴스
+        System.out.println("a.age = " + a.age); //55
+        System.out.println("b.age = " + b.age); //5
 
         b.name = "key";
         System.out.println("a.name = " + a.name);
@@ -25,6 +26,6 @@ public class C01Field {
 class MyClass01 {
     //field: 각 객체가 가질 수 있는 값 목록
     int age = 5;
-    String name = "moniter";
+    String name = "monitor";
 }
 
