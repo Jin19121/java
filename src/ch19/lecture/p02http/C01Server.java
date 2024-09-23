99b1a09ae31250ef430d818dff2e567a0c7187ef
Jin19121 <jinkim19121@gmail.com>
2024-09-23 16:12:19 +0900

    데이터베이스 입출력

---
+++ src/ch19/lecture/p02http/C01Server.java
@@ -0,0 +1,67 @@
+package ch19.lecture.p02http;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//확인 후 수정
+public class C01Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9999);
+
+        //여러 client가 동시 접속 - 스레드
+        Socket socket = serverSocket.accept();
+
+        InputStream is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        OutputStream os = socket.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+        PrintWriter pw = new PrintWriter(bw);
+
+
+        //요청(client -> server)
+        try (br; isr; is) {
+            //첫 줄: method (GET), 경로, 버전
+            String line1 = br.readLine();
+            System.out.println(line1);
+
+            //두번째 줄~: header
+            String line2 = null;
+            while ((line2 = br.readLine()).equals("")) {
+                System.out.println(line2);
+            }
+
+            //3번째: 빈 줄
+
+            //빈 줄 이후: 요청 본문(request body)
+        }
+
+        //응답(server-> client)
+        try (pw; bw; osw; os) {
+            String html = """
+                    <html>
+                        <body>
+                            <h1>안녕 브라우저</h1>
+                            <p style = "color:red"> 첫 번째 만든 서버 프로그램이야</p>
+                            <p style="text-align: center"> 이렇게 어려울 줄 몰랐어.</p>
+                        </body>
+                    </html>""";
+
+            //첫번쨰 줄: 응답 상태 줄 (버전 상태코드, 상태텍스트)
+            pw.println("HTTP/1.1 200 OK");
+
+            //두번째 줄
+            pw.println("Content-type: text/html; charsate=utf-8");
+            pw.println("Content-Length: " + html.getBytes().length);
+
+            //빈줄
+            pw.println();
+
+            //응답본물...
+            pw.println(html);
+        }
+    }
+}
