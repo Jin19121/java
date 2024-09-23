0b838b35ef3093dd4715aa55c7d15099ec4513c9
Jin19121 <jinkim19121@gmail.com>
2024-09-23 17:45:10 +0900

    reflection

---
+++ src/ch12/lecture/p05annotation/C01Reflection.java
@@ -0,0 +1,27 @@
+package ch12.lecture.p05annotation;
+
+public class C01Reflection {
+    public static void main(String[] args) throws ClassNotFoundException {
+//reflection: 클래스 정보를 반영한 객체를 만들어 활용
+
+        // Class 객체 생성하여 활용
+
+        //Class 객체 만드는 법
+        //1. Class.forName()
+        Class o1 = Class.forName("java.lang.String");
+
+        //2. Class명.class
+        Class o2 = String.class;
+
+        //3. 객체명.getClass()
+        String a = "";
+        Class o3 = a.getClass();
+
+        System.out.println("o1 = " + o1);
+        System.out.println("o2 = " + o2);
+        System.out.println("o3 = " + o3);
+
+    }
+}
+
+
