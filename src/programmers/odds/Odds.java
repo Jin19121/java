da2109841b9ab8ff419bac63c5a4ade95653ef0a
Jin19121 <jinkim19121@gmail.com>
2024-10-08 09:08:02 +0900

    코딩 입문 문제 odds(미완)

---
+++ src/programmers/odds/Odds.java
@@ -0,0 +1,14 @@
+package programmers.odds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> odds = new ArrayList<>();
+
+
+        int[] answer = {};
+        return answer;
+    }
+}
