3ada246e17c276d846c37672c0f0b054f7b9ee43
Jin19121 <jinkim19121@gmail.com>
2024-10-08 17:53:00 +0900

    코딩 입문 문제 odds(완성)

--- src/programmers/odds/Odds.java
+++ src/programmers/odds/Odds.java
@@ -6,9 +6,16 @@ import java.util.List;
 class Solution {
     public int[] solution(int n) {
         List<Integer> odds = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                odds.add(i);
+            }
+        }
 
-
-        int[] answer = {};
+        int[] answer = new int[odds.size()];
+        for (int i = 0; i < odds.size(); i++) {
+            answer[i] = odds.get(i);
+        }
         return answer;
     }
 }
